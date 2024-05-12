1ffa73aafd6f8354bd843eb3b42d07ed87d7ff5d
Justin Hunt <bitwalkerjapan@gmail.com>
2024-05-12 20:22:00 +0900

    removed old region warnings about cant transcribe etc

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024021500;
+$plugin->version = 2024051200;
 $plugin->requires = 2016052300; // Moodle 3.1.
 $plugin->component = 'assignfeedback_cloudpoodll';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0.18 (Build 2024021500)';
+$plugin->release   = '1.0.19 (Build 2024051200)';
